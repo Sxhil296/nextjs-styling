80f27918a2314407c3eb5d0bd4dc624c2bd56868
Sahil <leviack629@gmail.com>
2023-02-25 12:37:56 +0530

    sass

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import styles from '../styles/About.module.css'
+// import styles from '../styles/About.module.css'
+import styles from '../styles/About.module.scss'
 
 const about = () => {
   return (
     <div>
-        <h2 className={styles.highlight}>About Page</h2>
+        <h2 className={styles.highlightscss}>About Page</h2>
         <button className='btn btn-primary'>Primary</button>
     </div>
   )
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import styles from '../styles/Contact.module.css'
+// import styles from '../styles/Contact.module.css'
+import styles from '../styles/Contact.module.scss'
 
 const contact = () => {
   return (
     <div>
         <h2 className={styles.highlight}>Contact Page</h2>
+        <h3 className={styles.highlightscss}>Hello</h3>
     </div>
   )
 }
