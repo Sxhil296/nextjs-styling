3b135e9e7efad1708095deec7e9c59ffc134d202
Sahil <leviack629@gmail.com>
2023-02-25 12:17:56 +0530

    bootstrap

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h2>About Page</h2>
+        <button className='btn btn-primary'>Primary</button>
+    </div>
+  )
+}
+
+export default about
